2ec76c9ab1da01ca6ec41e56e3e8d250f13623b6
Hugo Luis <hugoluis.system@gmail.com>
2021-01-26 16:16:19 -0500

    Add project files.

---
+++ Tarea001/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea001
+{
+    class Producto
+    {
+        public string Marca {  get; set; }
+        public string Descripcion { get; set; }
+        public double PesoEnKilogramos { get; set; }
+        public double PesoEnLibra 
+        {
+            get { return PesoEnKilogramos * 2.2046; }
+        }
+        
+    }
+}
---
+++ Tarea001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tarea001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var producto = new Producto();
+            producto.Marca = "Samsung";
+            producto.Descripcion = "Televisor LED";
+            producto.PesoEnKilogramos = 8;
+            Console.WriteLine(
+                $"Hemos creado un nuevo producto de la Marca = {producto.Marca.ToUpper()} \n" +
+                $"Descripción del Producto = {producto.Descripcion} \n" +
+                $"Este producto tiene un peso de  = {producto.PesoEnKilogramos} {"kg"} \n" +
+                $"En Libras de = {producto.PesoEnLibra}{"lb"} \n");
+            ;
+
+        }
+    }
+}
